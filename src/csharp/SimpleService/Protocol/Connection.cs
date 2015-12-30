d1473588ab802170726be233658e37c46457db40
Alan Doherty <alan.fred.doherty@googlemail.com>
2015-12-30 17:36:15 +0000

    Added node package

--- src/csharp/SimpleService/Protocol/Connection.cs
+++ src/csharp/SimpleService/Protocol/Connection.cs
@@ -50,24 +50,6 @@ namespace SimpleService.Protocol
                 return packetsIn.Count > 0;
             }
         }
-
-        /// <summary>
-        /// Gets if the client is connected.
-        /// </summary>
-        public bool Connected {
-            get {
-                return peer.Connected;
-            }
-        }
-
-        /// <summary>
-        /// Gets the underlying client thread.
-        /// </summary>
-        public Thread Thread {
-            get {
-                return thread;
-            }
-        }
         #endregion
 
         #region Methods
