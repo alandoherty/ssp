894da6860363d38e97c4334d332847ca3d0b4bb9
Alan Doherty <alan.fred.doherty@googlemail.com>
2015-12-31 18:11:14 +0000

    Initialized node package

---
+++ src/node/README.md
@@ -0,0 +1,9 @@
+[![npm version](https://badge.fury.io/js/simpleservice.svg)](https://badge.fury.io/js/simpleservice)
+
+A simple protocol for cross-language JSON communication
+
+# Installing
+
+```bash
+$ npm install simpleservice
+```
---
+++ src/node/src/index.js
@@ -0,0 +1,17 @@
+/**
+     __________ ____
+    / ___/ ___// __ \
+   \__ \\__ \/ /_/ /
+  ___/ /__/ / ____/
+ /____/____/_/
+
+ @author Alan Doherty
+ @purpose Exports the module.
+ */
+
+var ssp = {
+
+};
+
+// export
+module.exports = ssp;
